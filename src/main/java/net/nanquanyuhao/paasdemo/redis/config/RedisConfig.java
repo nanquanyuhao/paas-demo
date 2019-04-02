0024918c6645f2038335555399a01253e2215c3c
李业恒 <liyh1928@chinaunicom.cn>
2019-04-02 18:10:18 +0800

    添加redis示例及测试代码

---
+++ src/main/java/net/nanquanyuhao/paasdemo/redis/config/RedisConfig.java
@@ -0,0 +1,36 @@
+package net.nanquanyuhao.paasdemo.redis.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * Created by nanquanyuhao on 2018/11/17.
+ * <p>
+ * 完成对 Redis 的整合的一些配置
+ */
+@Configuration
+public class RedisConfig {
+
+    /**
+     * 使用 spring-boot-starter-data-redis 依赖已经具备初始化的 JedisConnectionFactory ，
+     * 仅需要再创建 RedisTemplate 即可
+     *
+     * @return
+     */
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(JedisConnectionFactory factory) {
+
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        // 关联
+        template.setConnectionFactory(factory);
+        // 为 key 设置序列化器
+        template.setKeySerializer(new StringRedisSerializer());
+        // 为 value 设置序列化器
+        template.setValueSerializer(new StringRedisSerializer());
+
+        return template;
+    }
+}
